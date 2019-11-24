1a150a9adee13ae32dba34332ccab1cfd1288ada
עמדת תלמיד <712@RUPPINDOM>
2019-11-24 17:06:01 +0200

    second commit

--- flights/Controllers/FlightController.cs
+++ flights/Controllers/FlightController.cs
@@ -18,7 +18,7 @@ namespace flights.Controllers
         // GET api/<controller>/5
         public string Get(int id)
         {
-            return "value";
+            return "reut";
         }
 
         // POST api/<controller>
